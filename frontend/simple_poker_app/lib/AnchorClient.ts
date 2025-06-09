14775a6a398254bc7964f3a9ea966425efe3e394
Bharath <reddybharath932@gmail.com>
2025-06-10 02:42:46 +0530

    Updated the client functions

--- frontend/simple_poker_app/lib/AnchorClient.ts
+++ frontend/simple_poker_app/lib/AnchorClient.ts
@@ -10,6 +10,7 @@ import {
 } from "@solana/web3.js";
 import { SimplePoker } from "./simple_poker";
 import idl from "./simple_poker.json";
+import { type Game } from "@/lib/types";
 const connection = new Connection("http://127.0.0.1:8899", "confirmed");
 
 export function useProgram() {
@@ -69,7 +70,8 @@ export async function CreateGame(
     program: anchor.Program<SimplePoker>,
     stakeAmount: anchor.BN,
     maxPlayers: number,
-) {
+    description: string,
+): Promise<Game | null> {
     if (!program.provider) {
         throw new Error("Provider is not configured.");
     }
@@ -81,9 +83,10 @@ export async function CreateGame(
         program.programId,
     );
     const lobbyAccountData = await program.account.gameLobby.fetch(lobbyPDA);
+    const current_game_id = lobbyAccountData.currentGameId;
 
     const [gamePDA] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("game"), new anchor.BN(lobbyAccountData.currentGameId).toBuffer("le", 8)],
+        [Buffer.from("game"), current_game_id.toArrayLike(Buffer, "le", 8)],
         program.programId,
     );
 
@@ -91,7 +94,7 @@ export async function CreateGame(
 
     if (gameAccountInfo) {
         console.log("Game already initialized. Please Join the game or wait for it to complete");
-        return;
+        return null;
     }
 
     try {
@@ -107,6 +110,19 @@ export async function CreateGame(
             `Game ID: ${gameAccountData.id}`,
             `Tx: ${tx_signature}`
         );
+
+        const createdGame: Game = {
+            id: gameAccountData.id.toString(),
+            description: description,
+            stakingAmount: Number(stakeAmount) / anchor.web3.LAMPORTS_PER_SOL,
+            maxPlayers: maxPlayers,
+            currentPlayers: gameAccountData.playerCount,
+            prizePool: (Number(gameAccountData.prizePool) / anchor.web3.LAMPORTS_PER_SOL),
+            status: gameAccountData.state.toString(),
+        }
+
+        return createdGame;
+
     } catch (error) {
         console.error("Failed create game", error);
         throw error;
@@ -115,7 +131,7 @@ export async function CreateGame(
 
 export async function JoinGame(
     program: anchor.Program<SimplePoker>,
-) {
+): Promise<Game | null> {
     if (!program.provider) {
         throw new Error("Provider is not configured.");
     }
@@ -130,13 +146,13 @@ export async function JoinGame(
     const lobbyAccountData = await program.account.gameLobby.fetch(lobbyPDA);
 
     const [gamePDA] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("game"), new anchor.BN(lobbyAccountData.currentGameId).toBuffer("le", 8)],
+        [Buffer.from("game"), new anchor.BN(lobbyAccountData.currentGameId).toArrayLike(Buffer, "le", 8)],
         program.programId,
     );
     const gameAccountInfo = await program.account.game.getAccountInfo(gamePDA);
 
     const [vaultPDA] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("game_vault"), lobbyAccountData.currentGameId.toBuffer("le", 8)],
+        [Buffer.from("game_vault"), lobbyAccountData.currentGameId.toArrayLike(Buffer, "le", 8)],
         program.programId,
     );
 
@@ -146,7 +162,7 @@ export async function JoinGame(
 
     if (!gameAccountInfo) {
         console.log("Game not created yet, please create the game first");
-        return;
+        return null;
     }
 
     try {
@@ -157,21 +173,23 @@ export async function JoinGame(
             })
             .rpc();
         const gameAccountData = await program.account.game.fetch(gamePDA);
+        //UpdateState of the game
         console.log(
             "Joined Game Sucessfully!",
             `User: ${provider.wallet.publicKey.toBase58()}`,
             `Game ID: ${gameAccountData.id}`,
             `Tx: ${tx_signature}`
         );
+        return mapOnChainDataToGame(gameAccountData);
     } catch (error) {
         console.error("Failed create game", error);
-        throw error;
+        return null
     }
 }
 
 export async function DetermineWinner(
     program: anchor.Program<SimplePoker>,
-) {
+): Promise<Game | null> {
     if (!program.provider) {
         throw new Error("Provider is not configured.");
     }
@@ -185,14 +203,14 @@ export async function DetermineWinner(
     const lobbyAccountData = await program.account.gameLobby.fetch(lobbyPDA);
 
     const [gamePDA] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("game"), new anchor.BN(lobbyAccountData.currentGameId).toBuffer("le", 8)],
+        [Buffer.from("game"), new anchor.BN(lobbyAccountData.currentGameId).toArrayLike(Buffer, "le", 8)],
         program.programId,
     );
 
     const gameAccountInfo = await program.account.game.getAccountInfo(gamePDA);
 
     const [vaultPDA] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("game_vault"), lobbyAccountData.currentGameId.toBuffer("le", 8)],
+        [Buffer.from("game_vault"), lobbyAccountData.currentGameId.toArrayLike(Buffer, "le", 8)],
         program.programId,
     );
 
@@ -202,7 +220,7 @@ export async function DetermineWinner(
 
     if (!gameAccountInfo) {
         console.log("Game not created yet, please create the game first");
-        return;
+        return null;
     }
 
     try {
@@ -220,21 +238,22 @@ export async function DetermineWinner(
             `Amount Won: ${gameAccountData.prizePool}`,
             `Tx: ${tx_signature}`
         );
+        return mapOnChainDataToGame(gameAccountData);
     } catch (error) {
         console.error("Failed create game", error);
-        throw error;
+        return null;
     }
 }
 
 export async function ClaimWinnings(
     program: anchor.Program<SimplePoker>,
-) {
+): Promise<Game | null> {
     if (!program.provider) {
         throw new Error("Provider is not configured.");
     }
 
     const provider = program.provider as anchor.AnchorProvider;
-    
+
     const [lobbyPDA] = PublicKey.findProgramAddressSync(
         [anchor.utils.bytes.utf8.encode("game_lobby")],
         program.programId,
@@ -243,7 +262,7 @@ export async function ClaimWinnings(
     const lobbyAccountData = await program.account.gameLobby.fetch(lobbyPDA);
 
     const [gamePDA] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("game"), new anchor.BN(lobbyAccountData.currentGameId).toBuffer("le", 8)],
+        [Buffer.from("game"), new anchor.BN(lobbyAccountData.currentGameId).toArrayLike(Buffer, "le", 8)],
         program.programId,
     );
 
@@ -251,19 +270,19 @@ export async function ClaimWinnings(
     const gameAccountInfo = await program.account.game.getAccountInfo(gamePDA);
 
     const [vaultPDA] = anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("game_vault"), lobbyAccountData.currentGameId.toBuffer("le", 8)],
+        [Buffer.from("game_vault"), lobbyAccountData.currentGameId.toArrayLike(Buffer, "le", 8)],
         program.programId,
     );
 
-    console.log("Winner",gameAccountData.winner);
+    console.log("Winner", gameAccountData.winner?.toBase58());
     console.log("Game PDA:", gamePDA.toBase58());
     console.log("Vault PDA:", vaultPDA.toBase58());
 
     if (!gameAccountInfo) {
         console.log("Game not created yet, please create the game first");
-        return;
+        return null;
     }
-    if(gameAccountData.winner == provider.wallet.publicKey){
+    if (gameAccountData.winner?.toBase58() == provider.wallet.publicKey.toBase58()) {
         try {
             const tx_signature = await program.methods
                 .claimPrize()
@@ -279,12 +298,68 @@ export async function ClaimWinnings(
                 `Amount Won: ${gameAccountData.prizePool}`,
                 `Tx: ${tx_signature}`
             );
+            return mapOnChainDataToGame(gameAccountData);
         } catch (error) {
             console.error("Failed create game", error);
             throw error;
         }
     } else {
         console.log("Only the winner can claim the prize");
+        return null;
     }
+}
+
+const mapOnChainDataToGame = (account: any): Game => {
+    // The prize pool is the stake amount multiplied by the number of players.
+    let prizePool = account.prizePool.toNumber() / anchor.web3.LAMPORTS_PER_SOL;
+    if (account.prizePool.toNumber() === 0) {
+        prizePool = (account.stakeAmount.toNumber() * account.maxPlayers) / anchor.web3.LAMPORTS_PER_SOL;
+    }
+
+    return {
+        id: account.id.toString(),
+        description: `Game #${account.id.toString()}`,
+        stakingAmount: account.stakeAmount.toNumber() / anchor.web3.LAMPORTS_PER_SOL,
+        maxPlayers: account.maxPlayers,
+        currentPlayers: account.playerCount,
+        prizePool: prizePool,
+        status: Object.keys(account.state)[0]
+    };
+};
+
+export async function fetchAllGames(
+    program: anchor.Program<SimplePoker>,
+): Promise<Game[] | null> {
+    if (!program.provider) {
+        throw new Error("Provider is not configured.");
+    }
+
+    // const provider = program.provider as anchor.AnchorProvider;
+
+    const [lobbyPDA] = PublicKey.findProgramAddressSync(
+        [anchor.utils.bytes.utf8.encode("game_lobby")],
+        program.programId,
+    );
+
+    const lobbyAccountData = await program.account.gameLobby.fetch(lobbyPDA);
+    const current_game_id = lobbyAccountData.currentGameId;
+
+
+    // const [gamePDA] = anchor.web3.PublicKey.findProgramAddressSync(
+    //     [Buffer.from("game"), new anchor.BN(lobbyAccountData.currentGameId).toArrayLike(Buffer,"le", 8)],
+    //     program.programId,
+    // );
+    const games: Game[] = [];
+    const gameAccountInfo = await program.account.game.all();
+    for (let i = 0; i < gameAccountInfo.length; i++) {
+        games.push(mapOnChainDataToGame(gameAccountInfo[i].account));
+    }
+
+    console.log(gameAccountInfo[0])
+    console.log(`Game Account Info ${gameAccountInfo[0].account.id}`)
+    return games;
+
+
+    return null;
 }
 
